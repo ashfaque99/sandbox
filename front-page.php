7d3d63887520fa208a0ccbb515da454ccf38924c
Ashfaq Ali <ashfaq.alijatoi@gmail.com>
2019-04-27 04:58:17 -0700

    sandbox-repo

---
+++ front-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The front page template file
+ *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Sandbox
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+ 
+
+
+<?php
+get_footer();
